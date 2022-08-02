ebce9bc84af71458dd69f99e4baa3a456d4c1b47
egxn <afvelosas@correo.udistrital.edu.co>
2022-08-02 00:14:57 -0500

    Restore meta refresh

--- src/sever.ts
+++ src/sever.ts
@@ -7,6 +7,7 @@ const createHtmlContent = function (content: string) : string {
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="0.5">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🦑 Squid</title>
     <style>
